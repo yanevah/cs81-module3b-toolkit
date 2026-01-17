6bade4505538d994b1891e057f04f331cc465582
Nina <janev_nina_alexandra@student.smc.edu>
2026-01-17 11:19:59 -0800

    Add multiply function

--- math.js
+++ math.js
@@ -7,3 +7,14 @@ double(5)
 function square(n) {
   return n * n;
 }
+
+function isEven(n) {
+  return n % 2 === 0;
+}
+function isOdd(n) {
+  return n % 2 !== 0;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
